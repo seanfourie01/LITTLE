651924fa3ef8336e4f72a20f69bf354e06706a2e
seanfourie01 <seanfouri3@gmail.com>
2025-04-18 16:41:40 -0400

    Update ASTNode.java

--- ASTNode.java
+++ ASTNode.java
@@ -1,5 +1,15 @@
-import java.util.List;
+import java.util.*;
 
 public abstract class ASTNode {
-}
+    protected List<ASTNode> children = new ArrayList<>();
 
+    public void addChild(ASTNode child) {
+        children.add(child);
+    }
+
+    public List<ASTNode> getChildren() {
+        return children;
+    }
+
+    public abstract String toString(); // for pretty printing
+}
