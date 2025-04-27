5d82bc39fdee03ba83ba745ce6d11da84a8f453f
Dylan Muraco <dylanjmuraco@gmail.com>
2025-04-27 01:43:45 -0400

    tweaking shell script

--- compareOutStats.sh
+++ compareOutStats.sh
@@ -1,5 +1,5 @@
 ./Micro.sh testCases/inputs/"$1".micro > testCases/little/"$1".asm
 
 ./tinyNew ./testCases/little/"$1".asm
-echo "\n----------\n"
+printf "\n----------\n"
 ./tinyNew ./testCases/outputs/"$1".out
